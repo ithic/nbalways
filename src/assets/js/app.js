7e76e4280f2f322d5498fd81d828f5e74fe179c8
John Robert Rankin <rob@ithicine.com>
2020-05-22 16:22:09 -0300

    Update map styling

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -94,7 +94,7 @@ $('.facebook-share-button').click((event) => {
 
 mapboxgl.accessToken = 'pk.eyJ1IjoianJyYW5raW4iLCJhIjoiY2s5dnQ5bjVuMDJzMzNtcnI1dnJuOTlocyJ9.SQv9UqQvK05yKgK9Xgxxrw';
 var lastCoordinates = [-65.4894, 47.4170];
-var lastZoom = 7;
+var lastZoom = 5;
 var bounds = [
     [-69.138537, 44.343955], // Southwest coordinates
     [-63.446923, 48.405988] // Northeast coordinates
@@ -167,7 +167,7 @@ map.on('load', () => {
                 });
                 var popup = new mapboxgl.Popup({anchor: anchor, maxWidth: maxWidth})
                     .setLngLat(coordinates)
-                    .setHTML(`<div class="popup-content"><p class="main">${content}</p><span class="address"><svg class="pin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50.59 75.01"><path d="M25.09,0l-.21,0-.07,0A25.5,25.5,0,0,0,3.19,37.91V38a1.91,1.91,0,0,0,.22.53l0,0L23.59,73.94a2.1,2.1,0,0,0,2.87.79,2.07,2.07,0,0,0,.79-.79L47.09,38.6l.13-.19a2,2,0,0,0,.15-.41,1.34,1.34,0,0,0,.07-.18,25.75,25.75,0,0,0,3.15-12.19A25.49,25.49,0,0,0,25.31,0Zm.19,10.22a14.68,14.68,0,0,1,0,29.35,14.68,14.68,0,0,1,0-29.35Z"/></svg>${name} | ${address}</span></div><div class="image"><img src="${image}" /></div>`)
+                    .setHTML(`<div class="popup-content"><p class="main">${content}</p><span class="address"><svg class="pin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50.59 75.01"><path d="M25.09,0l-.21,0-.07,0A25.5,25.5,0,0,0,3.19,37.91V38a1.91,1.91,0,0,0,.22.53l0,0L23.59,73.94a2.1,2.1,0,0,0,2.87.79,2.07,2.07,0,0,0,.79-.79L47.09,38.6l.13-.19a2,2,0,0,0,.15-.41,1.34,1.34,0,0,0,.07-.18,25.75,25.75,0,0,0,3.15-12.19A25.49,25.49,0,0,0,25.31,0Zm.19,10.22a14.68,14.68,0,0,1,0,29.35,14.68,14.68,0,0,1,0-29.35Z"/></svg>${name}<span class="separator">|</span>${address}</span></div><div class="image"><img src="${image}" /></div>`)
                     .addTo(map);
                 popup.on('close', () => {
                     map.flyTo({
