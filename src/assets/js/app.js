aa1d2cc069264d3f73ae06790ef3fd8a41325948
John Robert Rankin <rob@ithicine.com>
2020-05-22 10:46:43 -0300

    Create initial phase 2 updates: map and content reorg

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -91,3 +91,95 @@ $('.facebook-share-button').click((event) => {
         quote: shareMessage[document.documentElement.lang.toLowerCase()]
       }, function(response){});
 });
+
+mapboxgl.accessToken = 'pk.eyJ1IjoianJyYW5raW4iLCJhIjoiY2s5dnQ5bjVuMDJzMzNtcnI1dnJuOTlocyJ9.SQv9UqQvK05yKgK9Xgxxrw';
+var lastCoordinates = [-65.4894, 47.4170];
+var lastZoom = 7;
+var bounds = [
+    [-69.138537, 44.343955], // Southwest coordinates
+    [-63.446923, 48.405988] // Northeast coordinates
+];
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/jrrankin/ck9x4ba8z0r501ipas6h035kc/draft',
+    center: lastCoordinates, // starting position [lng, lat],
+    zoom: lastZoom,
+    maxBounds: bounds
+});
+
+map.scrollZoom.disable();
+map.addControl(
+    new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true
+    })
+);
+map.addControl(new mapboxgl.NavigationControl());
+map.on('load', () => {
+    fetch("https://api.mapbox.com/datasets/v1/jrrankin/ck9vtcwy20dtj2lk4etqjm4rd/features?access_token=pk.eyJ1IjoianJyYW5raW4iLCJhIjoiY2s5dnQ5bjVuMDJzMzNtcnI1dnJuOTlocyJ9.SQv9UqQvK05yKgK9Xgxxrw&limit=200")
+    .then((response) => {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem fetching location data', response.status);
+            return;
+        }
+        response.json().then((data) => {
+            map.addSource('places', {
+                'type': 'geojson',
+                'data': data
+            });
+            map.addLayer({
+                'id': 'places',
+                'type': 'symbol',
+                'source': 'places',
+                'layout': {
+                    'icon-image': 'heart',
+                    'icon-allow-overlap': false,
+                    'icon-size': 0.5
+                }
+            });
+            map.on('click', 'places', (e) => {
+                lastZoom = map.getZoom();
+                lastCoordinates = map.getCenter();
+                var coordinates = e.features[0].geometry.coordinates.slice();
+                var name = e.features[0].properties.name;
+                var content = e.features[0].properties.content;
+                var attribution = e.features[0].properties.attribution;
+                var address = e.features[0].properties.address;
+                var image = e.features[0].properties.image;
+                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+                }
+                
+                map.flyTo({
+                    center: [coordinates[0] + 0.05, coordinates[1]],
+                    zoom: 12,
+                    essential: true // this animation is considered essential with respect to prefers-reduced-motion
+                });
+                var popup = new mapboxgl.Popup({anchor: 'bottom-left', maxWidth: 'none'})
+                    .setLngLat(coordinates)
+                    .setHTML(`<div class="popup-content"><p class="main">${content}<span class="attribution">${attribution}</span></p><span class="address"><svg class="pin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50.59 75.01"><path d="M25.09,0l-.21,0-.07,0A25.5,25.5,0,0,0,3.19,37.91V38a1.91,1.91,0,0,0,.22.53l0,0L23.59,73.94a2.1,2.1,0,0,0,2.87.79,2.07,2.07,0,0,0,.79-.79L47.09,38.6l.13-.19a2,2,0,0,0,.15-.41,1.34,1.34,0,0,0,.07-.18,25.75,25.75,0,0,0,3.15-12.19A25.49,25.49,0,0,0,25.31,0Zm.19,10.22a14.68,14.68,0,0,1,0,29.35,14.68,14.68,0,0,1,0-29.35Z"/></svg>${name} | ${address}</span></div><div class="image"><img src="${image}" /></div>`)
+                    .addTo(map);
+                popup.on('close', () => {
+                    map.flyTo({
+                        center: lastCoordinates,
+                        zoom: lastZoom,
+                        essential: true
+                    });
+                });
+            });
+
+            map.on('mouseenter', 'places', () => {
+                map.getCanvas().style.cursor = 'pointer';
+            });
+
+            map.on('mouseleave', 'places', () => {
+                map.getCanvas().style.cursor = '';
+            });
+        });
+    })
+    .catch((err) => {
+        console.log('error!', err);
+    })
+});
