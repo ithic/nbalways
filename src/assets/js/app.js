0da6d626a45c1c2efd043fbd7b268cdcb3053e4c
John Robert Rankin <rob@ithicine.com>
2020-05-26 16:19:24 -0300

    Add mobile styles, French map, GA events for JS triggers

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -101,7 +101,7 @@ var bounds = [
 ];
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/jrrankin/ck9x4ba8z0r501ipas6h035kc/draft',
+    style: 'mapbox://styles/jrrankin/ck9x4ba8z0r501ipas6h035kc',
     center: lastCoordinates, // starting position [lng, lat],
     zoom: lastZoom,
     maxBounds: bounds
@@ -116,9 +116,15 @@ map.addControl(
         trackUserLocation: true
     })
 );
+
+const datasets = {
+    'en': 'ck9vtcwy20dtj2lk4etqjm4rd',
+    'fr': 'ckaoa5dhb261w22rst74fvdzz'
+};
+
 map.addControl(new mapboxgl.NavigationControl());
 map.on('load', () => {
-    fetch("https://api.mapbox.com/datasets/v1/jrrankin/ck9vtcwy20dtj2lk4etqjm4rd/features?access_token=pk.eyJ1IjoianJyYW5raW4iLCJhIjoiY2s5dnQ5bjVuMDJzMzNtcnI1dnJuOTlocyJ9.SQv9UqQvK05yKgK9Xgxxrw&limit=200")
+    fetch(`https://api.mapbox.com/datasets/v1/jrrankin/${datasets[document.documentElement.lang.toLowerCase()]}/features?access_token=pk.eyJ1IjoianJyYW5raW4iLCJhIjoiY2s5dnQ5bjVuMDJzMzNtcnI1dnJuOTlocyJ9.SQv9UqQvK05yKgK9Xgxxrw&limit=200`)
     .then((response) => {
         if (response.status !== 200) {
             console.log('Looks like there was a problem fetching location data', response.status);
@@ -169,7 +175,10 @@ map.on('load', () => {
                 var popup = new mapboxgl.Popup({anchor: anchor, maxWidth: maxWidth})
                     .setLngLat(coordinates)
                     .setHTML(`<div class="popup-content"><p class="main">${content}</p><div class="address"><svg class="pin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50.59 75.01"><path d="M25.09,0l-.21,0-.07,0A25.5,25.5,0,0,0,3.19,37.91V38a1.91,1.91,0,0,0,.22.53l0,0L23.59,73.94a2.1,2.1,0,0,0,2.87.79,2.07,2.07,0,0,0,.79-.79L47.09,38.6l.13-.19a2,2,0,0,0,.15-.41,1.34,1.34,0,0,0,.07-.18,25.75,25.75,0,0,0,3.15-12.19A25.49,25.49,0,0,0,25.31,0Zm.19,10.22a14.68,14.68,0,0,1,0,29.35,14.68,14.68,0,0,1,0-29.35Z"/></svg><span><a href="${url}" target="_blank" rel="noopener nofollow">${name}</a></span><span>${address}</span></div><div class="image"><img src="/assets/img/map/${image}" /></div>`)
-                    .addTo(map);
+                    .addTo(map);                
+                
+                ga('send', 'event', 'Map', 'show', name);
+                
                 popup.on('close', () => {
                     map.flyTo({
                         center: lastCoordinates,
@@ -237,4 +246,6 @@ function startAudio(player) {
     const audio = $(player).find('audio')[0];
     $(player).addClass('playing');
     audio.play();
+    
+    ga('send', 'event', 'Sounds', 'play', $(player).find('audio').attr('id'));
 }
