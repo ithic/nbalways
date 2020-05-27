482d5f81a28bbb9e04788fdaecdfd2e3a55109ef
John Robert Rankin <rob@ithicine.com>
2020-05-27 17:29:28 -0300

    Add tooltips and cluster tooltips support

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -578,13 +578,17 @@ const mapdata = {
       }
 }
 
+var tooltip = null;
+
 map.addControl(new mapboxgl.NavigationControl());
 map.on('load', () => {
-    map.addSource('places', {
+    var dataSource = map.addSource('places', {
         'type': 'geojson',
-        'data': mapdata[document.documentElement.lang.toLowerCase()]
+        'data': mapdata[document.documentElement.lang.toLowerCase()],
+        'cluster': true,
+        clusterRadius: 80
     });
-    map.addLayer({
+    var layer = map.addLayer({
         'id': 'places',
         'type': 'symbol',
         'source': 'places',
@@ -621,21 +625,53 @@ map.on('load', () => {
             zoom: 12,
             essential: true // this animation is considered essential with respect to prefers-reduced-motion
         });
-        var popup = new mapboxgl.Popup({anchor: anchor, maxWidth: maxWidth})
-            .setLngLat(coordinates)
-            .setHTML(`<div class="popup-content"><p class="main">${content}</p><div class="address"><svg class="pin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50.59 75.01"><path d="M25.09,0l-.21,0-.07,0A25.5,25.5,0,0,0,3.19,37.91V38a1.91,1.91,0,0,0,.22.53l0,0L23.59,73.94a2.1,2.1,0,0,0,2.87.79,2.07,2.07,0,0,0,.79-.79L47.09,38.6l.13-.19a2,2,0,0,0,.15-.41,1.34,1.34,0,0,0,.07-.18,25.75,25.75,0,0,0,3.15-12.19A25.49,25.49,0,0,0,25.31,0Zm.19,10.22a14.68,14.68,0,0,1,0,29.35,14.68,14.68,0,0,1,0-29.35Z"/></svg><span><a href="${url}" target="_blank" rel="noopener nofollow">${name}</a></span><span>${address}</span></div><div class="image"><img src="/assets/img/map/${image}" /></div>`)
-            .addTo(map);                
-        
-        ga('send', 'event', 'Map', 'show', name);
-        
-        popup.on('close', () => {
-            map.flyTo({
-                center: lastCoordinates,
-                zoom: lastZoom,
-                essential: true
-            });
-        });
+        if (!e.features[0].properties.cluster) {
+          var popup = new mapboxgl.Popup({anchor: anchor, maxWidth: maxWidth, className: 'window'})
+              .setLngLat(coordinates)
+              .setHTML(`<div class="popup-content"><p class="main">${content}</p><div class="address"><svg class="pin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50.59 75.01"><path d="M25.09,0l-.21,0-.07,0A25.5,25.5,0,0,0,3.19,37.91V38a1.91,1.91,0,0,0,.22.53l0,0L23.59,73.94a2.1,2.1,0,0,0,2.87.79,2.07,2.07,0,0,0,.79-.79L47.09,38.6l.13-.19a2,2,0,0,0,.15-.41,1.34,1.34,0,0,0,.07-.18,25.75,25.75,0,0,0,3.15-12.19A25.49,25.49,0,0,0,25.31,0Zm.19,10.22a14.68,14.68,0,0,1,0,29.35,14.68,14.68,0,0,1,0-29.35Z"/></svg><span><a href="${url}" target="_blank" rel="noopener nofollow">${name}</a></span><span>${address}</span></div><div class="image"><img src="/assets/img/map/${image}" /></div>`)
+              .addTo(map);                
+          
+          ga('send', 'event', 'Map', 'show', name);
+          
+          popup.on('close', () => {
+              map.flyTo({
+                  center: lastCoordinates,
+                  zoom: lastZoom,
+                  essential: true
+              });
+          });
+        }
     });
+
+    layer.on('mouseover', 'places', (e) => {
+      var places = "";
+      if (e.features[0].properties.cluster) {
+        var clusterID = e.features[0].id;
+        var pointCount = e.features[0].properties.point_count;
+        var clusterSource = map.getSource('places');
+        clusterSource.getClusterLeaves(clusterID, pointCount, 0, (err, features) => {
+          var coordinates = features[0].geometry.coordinates;
+          features.forEach(feature => places += feature.properties.name + ", ");
+          places = places.slice(0, -2);
+          tooltip = new mapboxgl.Popup({anchor: 'bottom', className: 'tooltip'})
+                              .setLngLat(coordinates)
+                              .setHTML(places)
+                              .addTo(map);
+        });
+      } else {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        places = e.features[0].properties.name;
+
+        tooltip = new mapboxgl.Popup({anchor: 'bottom', className: 'tooltip'})
+                              .setLngLat(coordinates)
+                              .setHTML(places)
+                              .addTo(map);
+      }
+    });
+
+    layer.on('mouseleave', 'places', (e) => {
+      tooltip.remove();
+    })
 
     map.on('mouseenter', 'places', () => {
         map.getCanvas().style.cursor = 'pointer';
